0219108ec3fba631be18401a81534e01e2b28c05
Michael Zoech <michi.zoech@gmail.com>
2010-05-24 19:56:21 +0200

    add cookbook for servicemix

---
+++ scripts/devbox/cookbooks/servicemix/metadata.rb
@@ -0,0 +1,30 @@
+#
+# Copyright 2010, OpenEngSB Division, Vienna University of Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+maintainer       "OpenEngSB Division, Vienna University of Technology"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Installs/Configures ServiceMix"
+version          "0.1"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+%w{ java }.each do |d|
+  depends d
+end
+
